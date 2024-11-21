c034744c36d356de676e8840ad89b4f7200ff614
SeRaG <aserag0101@gmail.com>
2024-11-21 08:33:31 +0200

    save changes

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
+import home from './Fashop/Home/home';
 
 function App() {
   return (
     <div className="App">
-
+      <home />
     </div>
   );
 }
---
+++ app/src/Fashop/Home/home.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function home() {
+    return (
+        <div className='container'>
+            <div className='parent'>
+                <div className='detail'>
+                    <div className='icons'>
+                        <a href='#'>
+                            <i className='fa fa-facebook-square'></i>
+                        </a>
+                        <div className='Slogan'>
+                            {""}
+                            Hello my name is Ahmed
+                            <div />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
