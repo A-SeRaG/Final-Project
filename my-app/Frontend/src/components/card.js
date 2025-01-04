af5af77657c508b2f5692c80fe1189ad683d0da4
SeRaG <aserag0101@gmail.com>
2025-01-04 17:01:40 +0200

    organizing

---
+++ my-app/Frontend/src/components/card.js
@@ -0,0 +1,21 @@
+// card.js or card.jsx
+import React from "react";
+import { Card, Button } from "react-bootstrap";
+import Image from "react-bootstrap/esm/Image";
+
+const Cardcomponent = ({ product }) => {
+  return (
+    <Card id="card" style={{ width: "18rem" }}>
+      <Card.Body>
+        <Image src=" " fluid /><Card.Title></Card.Title>
+        <p>Description: </p>
+        <p>Price: </p>
+        <Button id="btn" variant="primary">
+          add to cart
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Cardcomponent;
